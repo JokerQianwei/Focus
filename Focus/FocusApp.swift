e9bab8d02b381b415b3f3dd95a884ee1fb50bad3
JokerQianwei <1174665346@qq.com>
2025-04-28 21:59:29 +0800

    Initial Commit

---
+++ Focus/FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusApp.swift
+//  Focus
+//
+//  Created by 杨乾巍 on 2025/4/28.
+//
+
+import SwiftUI
+
+@main
+struct FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
